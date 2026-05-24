40422855f6826ceb9d9bd713c699379564cbda94
AlphaSoftX <alphasoftx@gmail.com>
2026-05-24 15:17:16 +0530

    Week1 Warmup Exercise (Greedy_or_not) - Used NumPy to reduce time complexity by using vector operations - AutoGrader took 1.63s

--- Week1/Warmup/Greedy_or_not.py
+++ Week1/Warmup/Greedy_or_not.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 n = int(input())
 numbers = list(map(int, input().split()))
 
@@ -7,15 +9,16 @@ if len(numbers) != n:
 
 # assuming always the player 1 has first to move
 
-# 1D Array Memoization and Bottom-Up DP
-memo = numbers.copy()
+# 1D Array Memoization and Bottom-Up DP --> Using NumPy
+nums = np.array(numbers)
+memo = nums.copy()
 
-for length in range(2, n+1):
-  for start in range(n-length+1):
-    end = length+start-1
-    chooseFirst = numbers[start]-memo[start+1]
-    chooseLast = numbers[end]-memo[start]
-    memo[start] = max(chooseFirst, chooseLast)
+for length in range(2, n + 1):
+  starts = np.arange(n - length + 1)
+  ends = starts + length - 1
+  chooseFirst  = nums[starts] - memo[starts + 1]
+  chooseLast = nums[ends]   - memo[starts]
+  memo[starts] = np.maximum(chooseFirst, chooseLast)
 
 check = memo[0]
 
