077086d57a8a629915ad0dba73699076554309a2
AlphaSoftX <alphasoftx@gmail.com>
2026-05-24 13:04:12 +0530

    Week1 Warmup Exercise (Greedy_or_not) - 7 cases ran | 8th case 12s timeout error

--- Week1/Warmup/Greedy_or_not.py
+++ Week1/Warmup/Greedy_or_not.py
@@ -1,1 +1,30 @@
-# Write your code here
+n = int(input())
+numbers = list(map(int, input().split()))
+
+if len(numbers) != n:
+  print("Invalid input")
+  exit()
+
+# assuming always the player 1 has first to move
+
+def move(start, end):
+  global numbers
+
+  if start == end:
+    return numbers[start]
+  
+  chooseFirst = numbers[start]-move(start+1, end)
+  chooseLast = numbers[end]-move(start, end-1)
+
+  return max(chooseFirst, chooseLast)
+
+check = move(0, n-1)
+
+if check > 0:
+  print("Player 1 wins")
+elif check < 0:
+  print("Player 2 wins")
+else:
+  print("Its a draw")
+
+exit()
