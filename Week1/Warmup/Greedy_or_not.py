3b78a0f3ba443f28a103ebc157f515717bdcab5c
AlphaSoftX <alphasoftx@gmail.com>
2026-05-24 13:19:02 +0530

    Week1 Warmup Exercise (Greedy_or_not) - Added Memoization - 8 cases ran | 9th case 'maximum recursion depth exceeded' error

--- Week1/Warmup/Greedy_or_not.py
+++ Week1/Warmup/Greedy_or_not.py
@@ -1,5 +1,6 @@
 n = int(input())
 numbers = list(map(int, input().split()))
+memo = {}
 
 if len(numbers) != n:
   print("Invalid input")
@@ -7,16 +8,25 @@ if len(numbers) != n:
 
 # assuming always the player 1 has first to move
 
+# Memoization
+
 def move(start, end):
-  global numbers
+  global numbers, memo
 
   if start == end:
     return numbers[start]
+
+  tupleKey = (start, end)
+  
+  if tupleKey in memo:
+    return memo[tupleKey]
   
   chooseFirst = numbers[start]-move(start+1, end)
   chooseLast = numbers[end]-move(start, end-1)
 
-  return max(chooseFirst, chooseLast)
+  memo[tupleKey] = max(chooseFirst, chooseLast)
+
+  return memo[tupleKey]
 
 check = move(0, n-1)
 
