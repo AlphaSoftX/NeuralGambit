3f96999511b964458ba29b66362939729fae641f
AlphaSoftX <alphasoftx@gmail.com>
2026-05-24 14:28:59 +0530

    Week1 Warmup Exercise (Greedy_or_not) - Updated Memoization --> 1D - AutoGrader took 7.67s

--- Week1/Warmup/Greedy_or_not.py
+++ Week1/Warmup/Greedy_or_not.py
@@ -1,6 +1,5 @@
 n = int(input())
 numbers = list(map(int, input().split()))
-memo = {}
 
 if len(numbers) != n:
   print("Invalid input")
@@ -8,19 +7,17 @@ if len(numbers) != n:
 
 # assuming always the player 1 has first to move
 
-# Memoization and Bottom-Up DP
-
-for i in range(0,n):
-  memo[(i,i)] = numbers[i]
+# 1D Array Memoization and Bottom-Up DP
+memo = numbers.copy()
 
 for length in range(2, n+1):
   for start in range(n-length+1):
     end = length+start-1
-    chooseFirst = numbers[start]-memo[(start+1, end)]
-    chooseLast = numbers[end]-memo[(start, end-1)]
-    memo[(start, end)] = max(chooseFirst, chooseLast)
+    chooseFirst = numbers[start]-memo[start+1]
+    chooseLast = numbers[end]-memo[start]
+    memo[start] = max(chooseFirst, chooseLast)
 
-check = memo[(0, n-1)]
+check = memo[0]
 
 if check > 0:
   print("Player 1 wins")
